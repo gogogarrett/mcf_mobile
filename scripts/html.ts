1f23d8cdcaa11e17bbd3e4f7b91bca1ee5226ead
Garrett Heinlen <heinleng@gmail.com>
2013-01-16 10:01:08 -0600

    initial commit

--- scripts/html.ts
+++ scripts/html.ts
@@ -4,26 +4,25 @@ $("/html") {
   rewrite_links()
   absolutize_srcs()
 
-    
   # Add the mobile meta tags 
   clean_mobile_meta_tags()
 
-    
   # Needed to begin mobilizing
   remove_all_styles()
+  remove_scripts()
   remove_html_comments()
 
   # Late load all the images on the site
   # lateload()
-  
-
-
 
   add_assets()
 
   @import sections/header.ts
   @import sections/footer.ts
-
   @import mappings.ts
-}
 
+  # remove all inline styles
+  $("//*") {
+    remove("@style")
+  }
+}
--- scripts/sections/header.ts
+++ scripts/sections/header.ts
@@ -1,5 +1,29 @@
-# $('./body') {
-#   insert_top("header", class: "_header") {
-#     Move stuff here
-#   }
-# }
+$('./body') {
+  insert_top("header", class: "_header clearfix") {
+
+    insert("section", class: "top_banner") {
+
+      insert("a", "", href: "/users/1/edit", class: "settings")
+      
+    }
+
+  }
+
+  $$(".brand") {
+    move_to("//section[@class='top_banner']")
+  }
+
+  $$("#logged") {
+    move_to("//a[@id='logo']", "after")
+    insert("div", "", class: "clearfix")
+  }
+
+   $$("#top-dashboard") {
+    move_to("//a[@id='logo']", "before")
+    insert("div", "", class: "clearfix")
+  }
+  # $$(".contain") {remove()}
+  # $$("#header") { remove() }
+  # $$("#menu") { remove() }
+  # $$("#right-column") { remove() }
+}
